20925d6cce9ae8acf419380e406a92b1640ada0c
Andy Grecu <andygrecu2000@gmail.com>
2023-01-29 14:27:22 +0200

    first commit

---
+++ src/main/java/com/ase/ebuss/iad/IadApplication.java
@@ -0,0 +1,13 @@
+package com.ase.ebuss.iad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ase/ebuss/iad/IadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ase.ebuss.iad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
